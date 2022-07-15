6950fff22d3f2422570e32e2510a400e80a45948
Yuying0909 <femgling_123@163.com>
2022-07-15 21:17:10 +0800

    test

--- 1.py
+++ 1.py
@@ -7,8 +7,5 @@ c=np.zeros((x.shape[0],x.shape[1],3))
 c[:,:,0]=x
 c[:,:,1]=y
 c[::2,::2,2]=1
-cv.imwrite(r'/Users/yuying/Desktop/YY/PythonProject/test/1.jpg',
-           np.stack([np.uint8(c[:,:,0]*10),np.uint8(c[:,:,1]*10),np.uint8(c[:,:,2]*255)],axis=-1))
-
-
-
+cv.imwrite(r'/Users/yuying/Desktop/YY/PythonProject/test/2.jpg',
+           np.stack([np.uint8(c[:, :, 0] * 10), np.uint8(c[:, :, 1] * 10), np.uint8(c[:, :, 2] * 255)], axis=-1))
