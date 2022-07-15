20512b5d6571bbacba00863b77b813238f1e4fe2
Yuying0909 <femgling_123@163.com>
2022-07-15 20:34:14 +0800

    test

---
+++ 1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+
+
+x,y=np.meshgrid(np.arange(10),np.arange(8))
+c=np.zeros((x.shape[0],x.shape[1],3))
+c[:,:,0]=x
+c[:,:,1]=y
+c[::2,::2,2]=1
+cv.imwrite(r'/Users/yuying/Desktop/YY/PythonProject/test/1.jpg',
+           np.stack([np.uint8(c[:,:,0]*10),np.uint8(c[:,:,1]*10),np.uint8(c[:,:,2]*255)],axis=-1))
+
+
+
