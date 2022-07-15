3fe640264a38eee42c518fb203099b5efee2ddff
Yuying0909 <femgling_123@163.com>
2022-07-15 21:59:10 +0800

    test

--- 1.py
+++ 1.py
@@ -1,11 +1,5 @@
 import numpy as np
-import cv2 as cv
+a=np.arange(20)
+print(a)
 
 
-x,y=np.meshgrid(np.arange(10),np.arange(8))
-c=np.zeros((x.shape[0],x.shape[1],3))
-c[:,:,0]=x
-c[:, :, 1] = y
-c[::2, ::2, 2] = 3
-cv.imwrite(r'/Users/yuying/Desktop/YY/PythonProject/test/2.jpg',
-           np.stack([np.uint8(c[:, :, 0] * 10), np.uint8(c[:, :, 1] * 10), np.uint8(c[:, :, 2] * 255)], axis=-1))
